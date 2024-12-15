ce6043577e98e5ac1a0eb5397a42f1c1df046235
Ferdinand Andhika Widhiyan <9981ferdinand@gmail.com>
2024-12-16 00:21:00 +0700

    1

--- api/vercel.php
+++
@@ -1,7 +0,0 @@
-<?php
-declare(strict_types=1);
-
-if (isset($_ENV['VERCEL_DEPLOYMENT_URL'])) {
-    $_SERVER['HTTP_HOST'] = $_ENV['VERCEL_DEPLOYMENT_URL'];
-    $_SERVER['SERVER_NAME'] = $_ENV['VERCEL_DEPLOYMENT_URL'];
-} 
